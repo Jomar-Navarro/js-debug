96383eda968c9c906ecdd01cede13c2a4eb321cb
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-03-08 16:15:03 +0100

    [add] bundle 2 es 3 done

--- bundle-02/bundle-02/main.js
+++ bundle-02/bundle-02/main.js
@@ -11,9 +11,22 @@
 
 
 // ESERCIZIO 1
+// function checkAge() {
+//     const myAge = 32;
+//     const message = '';
+
+//     if (myAge < 18) {
+//         message = `Sei troppo giovane! Hai ${myAge} anni!`;
+//     } else {
+//         message = 'Hai più di 18 anni!';
+//     }
+// }
+// checkAge();
+
+// Correzione
 function checkAge() {
     const myAge = 32;
-    const message = '';
+    let message = '';
 
     if (myAge < 18) {
         message = `Sei troppo giovane! Hai ${myAge} anni!`;
@@ -25,8 +38,8 @@ checkAge();
 
 /*
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-1. 
-2. 
+1. Questo codice é una funzione con all'interno un'istruzione di condizione
+2. Qua c'é un errore di sinstassi perché message é stata assegnata una variabile costante, quindi non puó essere modificata. La giusta correzione é assegnare la variabile message con un let che é possibile modificare.
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 */
 
@@ -37,6 +50,20 @@ function printColorsNumber() {
 }
 printColorsNumber();
 
+// Correzione
+function printColorsNumber() {
+    const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+    console.log(`Nella mia palette ci sono ${colors.length} colori!`);
+}
+printColorsNumber();
+
+/*
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+1. Questo codice é una funzione che mi stampa il numero di colori in base a quanti c'é ne sono in un array
+2. In questo codice c'é un errore di parola non é "lenght" ma "length".
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
+
 
 // ESERCIZIO 3
 function addNumbers() {
@@ -46,6 +73,22 @@ function addNumbers() {
     console.log(`Il risultato finale è ${total}`);
 }
 addNumbers();
+
+// Correzione
+function addNumbers() {
+    const userNumber = parseInt(prompt('Inserisci un numero'));
+    const total = userNumber + 12;
+
+    console.log(`Il risultato finale è ${total}`);
+}
+addNumbers();
+
+/*
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+1. Questa é una funzioone che con un promt mi chiede un numero e lo stampa in console
+2. L'errore qua é nella variabile, perché il valore assegnato viene rilevato come una stringa e non come un numero. Quindi mi concatenava il numero che assegna l'utente con il "total". La correzione giusta é aggiugere un parsseInt.
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
 
 
 // ESERCIZIO 4
@@ -67,60 +110,46 @@ function checkAccess() {
 }
 checkAccess();
 
+/*
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+1. 
+2. 
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
+
 
 // ESERCIZIO 5 (suggerimento: c'è un solo errore)
-function checkAccessImproved() {
-    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+// function checkAccessImproved() {
+//     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
 
-    const userEmail = prompt('Inserisci il tuo indirizzo email');
+//     const userEmail = prompt('Inserisci il tuo indirizzo email');
 
-    let grantAccess = 'false';
+//     let grantAccess = 'false';
 
-    for (let i = 0; i < addresses.length; i++) {
-        const email = addresses[i];
+//     for (let i = 0; i < addresses.length; i++) {
+//         const email = addresses[i];
 
-        if (userEmail.length > 5) {
+//         if (userEmail.length > 5) {
 
-            if (email === userEmail) {
-                grantAccess = 'true';
+//             if (email === userEmail) {
+//                 grantAccess = 'true';
 
-            }
+//             }
 
-        }
+//         }
 
-        if (grantAccess) {
-            console.log('Accesso consentito!');
-        } else {
-            console.log('Accesso negato!');
-        }
-    }
-    checkAccessImproved();
+//         if (grantAccess) {
+//             console.log('Accesso consentito!');
+//         } else {
+//             console.log('Accesso negato!');
+//         }
+//     }
+// }
+//     checkAccessImproved();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+1. 
+2. 
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
